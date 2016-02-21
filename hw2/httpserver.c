e309802ddf534411bc385aeeb1071b2fbef1512f
yl2612 <yl2695@nyu.edu>
2016-02-21 18:59:21 -0500

    support GET /index.html

--- hw2/httpserver.c
+++ hw2/httpserver.c
@@ -39,22 +39,28 @@ int server_proxy_port;
  *      of files in the directory with links to each.
  *   4) Send a 404 Not Found response.
  */
-void handle_files_request(int fd) {
+void handle_files_request(int fd)
+{
 
-  /* YOUR CODE HERE (Feel free to delete/modify the existing code below) */
+  /* YOUR CODE HERE */
 
   struct http_request *request = http_request_parse(fd);
-
-  http_start_response(fd, 200);
-  http_send_header(fd, "Content-type", "text/html");
-  http_end_headers(fd);
-  http_send_string(fd,
-      "<center>"
-      "<h1>Welcome to httpserver!</h1>"
-      "<hr>"
-      "<p>Nothing's here yet.</p>"
-      "</center>");
-
+  struct stat s;
+  char *path = strcat(server_files_directory, request->path);
+  if (stat(path, &s) == 0) {
+    if(s.st_mode & S_IFREG) {
+      FILE *file = fopen(path, "r"); 
+      char *type = http_get_mime_type(path);
+      http_start_response(fd, 200);
+      http_send_header(fd, "Content-type", type);
+      http_end_headers(fd);
+      char c[100];
+      while (fgets(c, 100, file) != NULL) {
+        http_send_string(fd, c);
+      }
+      fclose (file);
+    } 
+  }
 }
 
 /*
@@ -68,7 +74,8 @@ void handle_files_request(int fd) {
  *   | client | <-> | httpserver | <-> | proxy target |
  *   +--------+     +------------+     +--------------+
  */
-void handle_proxy_request(int fd) {
+void handle_proxy_request(int fd)
+{
 
   /* YOUR CODE HERE */
 
@@ -79,7 +86,7 @@ void handle_proxy_request(int fd) {
  * the fd number of the server socket in *socket_number. For each accepted
  * connection, calls request_handler with the accepted fd number.
  */
-void serve_forever(int *socket_number, void (*request_handler)(int)) {
+void serve_forever(int* socket_number, void (*request_handler)(int)) {
 
   struct sockaddr_in server_address, client_address;
   size_t client_address_length = sizeof(client_address);
@@ -88,14 +95,14 @@ void serve_forever(int *socket_number, void (*request_handler)(int)) {
 
   *socket_number = socket(PF_INET, SOCK_STREAM, 0);
   if (*socket_number == -1) {
-    perror("Failed to create a new socket");
+    fprintf(stderr, "Failed to create a new socket: error %d: %s\n", errno, strerror(errno));
     exit(errno);
   }
 
   int socket_option = 1;
   if (setsockopt(*socket_number, SOL_SOCKET, SO_REUSEADDR, &socket_option,
         sizeof(socket_option)) == -1) {
-    perror("Failed to set socket options");
+    fprintf(stderr, "Failed to set socket options: error %d: %s\n", errno, strerror(errno));
     exit(errno);
   }
 
@@ -104,14 +111,14 @@ void serve_forever(int *socket_number, void (*request_handler)(int)) {
   server_address.sin_addr.s_addr = INADDR_ANY;
   server_address.sin_port = htons(server_port);
 
-  if (bind(*socket_number, (struct sockaddr *) &server_address,
+  if (bind(*socket_number, (struct sockaddr*) &server_address,
         sizeof(server_address)) == -1) {
-    perror("Failed to bind on socket");
+    fprintf(stderr, "Failed to bind on socket: error %d: %s\n", errno, strerror(errno));
     exit(errno);
   }
 
   if (listen(*socket_number, 1024) == -1) {
-    perror("Failed to listen on socket");
+    fprintf(stderr, "Failed to listen on socket: error %d: %s\n", errno, strerror(errno));
     exit(errno);
   }
 
@@ -119,30 +126,27 @@ void serve_forever(int *socket_number, void (*request_handler)(int)) {
 
   while (1) {
 
-    client_socket_number = accept(*socket_number,
-        (struct sockaddr *) &client_address,
-        (socklen_t *) &client_address_length);
+    client_socket_number = accept(*socket_number, (struct sockaddr*) &client_address,
+        (socklen_t*) &client_address_length);
     if (client_socket_number < 0) {
-      perror("Error accepting socket");
+      fprintf(stderr, "Error accepting socket: error %d: %s\n", errno, strerror(errno));
       continue;
     }
 
-    printf("Accepted connection from %s on port %d\n",
-        inet_ntoa(client_address.sin_addr),
+    printf("Accepted connection from %s on port %d\n", inet_ntoa(client_address.sin_addr),
         client_address.sin_port);
 
     pid = fork();
     if (pid > 0) {
       close(client_socket_number);
     } else if (pid == 0) {
-      // Un-register signal handler (only parent should have it)
-      signal(SIGINT, SIG_DFL);
+      signal(SIGINT, SIG_DFL); // Un-register signal handler (only parent should have it)
       close(*socket_number);
       request_handler(client_socket_number);
       close(client_socket_number);
       exit(EXIT_SUCCESS);
     } else {
-      perror("Failed to fork child");
+      fprintf(stderr, "Failed to fork child: error %d: %s\n", errno, strerror(errno));
       exit(errno);
     }
   }
@@ -156,12 +160,11 @@ void signal_callback_handler(int signum) {
   printf("Caught signal %d: %s\n", signum, strsignal(signum));
   printf("Closing socket %d\n", server_fd);
   if (close(server_fd) < 0) perror("Failed to close server_fd (ignoring)\n");
-  exit(0);
+  exit(signum);
 }
 
-char *USAGE =
-  "Usage: ./httpserver --files www_directory/ --port 8000\n"
-  "       ./httpserver --proxy inst.eecs.berkeley.edu:80 --port 8000\n";
+char *USAGE = "Usage: ./httpserver --files www_directory/ --port 8000\n"
+              "       ./httpserver --proxy inst.eecs.berkeley.edu:80 --port 8000\n";
 
 void exit_with_usage() {
   fprintf(stderr, "%s", USAGE);
@@ -173,10 +176,16 @@ int main(int argc, char **argv) {
 
   /* Default settings */
   server_port = 8000;
-  void (*request_handler)(int) = NULL;
+  server_files_directory = malloc(1024);
+  getcwd(server_files_directory, 1024);
+  server_proxy_hostname = "inst.eecs.berkeley.edu";
+  server_proxy_port = 80;
+
+  void (*request_handler)(int) = handle_files_request;
 
   int i;
-  for (i = 1; i < argc; i++) {
+  for (i = 1; i < argc; i++)
+  {
     if (strcmp("--files", argv[i]) == 0) {
       request_handler = handle_files_request;
       free(server_files_directory);
@@ -216,12 +225,6 @@ int main(int argc, char **argv) {
       fprintf(stderr, "Unrecognized option: %s\n", argv[i]);
       exit_with_usage();
     }
-  }
-
-  if (server_files_directory == NULL && server_proxy_hostname == NULL) {
-    fprintf(stderr, "Please specify either \"--files [DIRECTORY]\" or \n"
-                    "                      \"--proxy [HOSTNAME:PORT]\"\n");
-    exit_with_usage();
   }
 
   serve_forever(&server_fd, request_handler);
